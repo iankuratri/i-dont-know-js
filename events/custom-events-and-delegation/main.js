bb4746ff240772b8067ea9f956d32add523ac1ec
Ankur Atri <ankuratri.in@gmail.com>
2021-03-08 11:32:31 +0530

    event delegation and custom event example done

--- events/custom-events-and-delegation/main.js
+++ events/custom-events-and-delegation/main.js
@@ -2,7 +2,7 @@ const shoppingFormEl = document.querySelector("form[name='shopping']");
 const listEl = document.querySelector("#list");
 // custom event
 const itemsUpdatedEvent = new CustomEvent("itemsUpdated");
-const itemList = [];
+let itemList = [];
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -34,11 +34,13 @@ function displayItems() {
             id="${item.id}" 
             ${item.completed && "checked"}
             />
-            <span>
+            <span class="itemName">
             ${item.name}
             </span>
         </div>
-        <button>x</button>
+        <button aria-label="Remove Item" id="${item.id}">
+          &#10005
+        </button>
     </li>`
     )
     .join("");
@@ -46,5 +48,50 @@ function displayItems() {
   listEl.innerHTML = itemListHtml;
 }
 
-window.addEventListener("submit", handleSubmit);
+function mirrorToLocalStorage() {
+  localStorage.setItem("items", JSON.stringify(itemList));
+}
+
+function restoreFromLocalStorage() {
+  const savedItemList = JSON.parse(localStorage.getItem("items"));
+
+  if (!(savedItemList && savedItemList.length)) return;
+
+  itemList = savedItemList;
+
+  listEl.dispatchEvent(itemsUpdatedEvent);
+}
+
+function deleteItem(id) {
+  itemList = itemList.filter((item) => item.id !== id);
+
+  listEl.dispatchEvent(itemsUpdatedEvent);
+}
+
+function markAsComplete(id) {
+  const item = itemList.find((item) => item.id === id);
+  item.completed = !item.completed;
+
+  listEl.dispatchEvent(itemsUpdatedEvent);
+}
+
+shoppingFormEl.addEventListener("submit", handleSubmit);
 listEl.addEventListener("itemsUpdated", displayItems);
+listEl.addEventListener("itemsUpdated", mirrorToLocalStorage);
+
+// Event Delegation:
+// We listen to click on the list <ul> but then delegate
+// the click over to the button if that is what was clicked
+listEl.addEventListener("click", function (event) {
+  const id = parseInt(event.target.id);
+
+  if (event.target.matches("button")) {
+    deleteItem(id);
+  }
+
+  if (event.target.matches("input[type='checkbox']")) {
+    markAsComplete(id);
+  }
+});
+
+restoreFromLocalStorage();
