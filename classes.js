f9b4e558f2495868ca0ee79726f4a8fec7ba7b79
Ankur Atri <ankuratri.in@gmail.com>
2021-04-23 10:50:58 +0530

    class defination and static method added

--- classes.js
+++ classes.js
@@ -1,3 +1,10 @@
+/**
+ * Classes
+ *
+ * Classes are a template for creating objects.
+ * They encapsulate data with code to work on that data.
+ */
+
 class Student {
   constructor(name) {
     this.name = name;
@@ -6,7 +13,26 @@ class Student {
   study() {
     console.log(`${this.name} studies`);
   }
+
+  /**
+   * static
+   *
+   * The static keyword defines a static method or property for a class.
+   * Neither static methods nor static properties can be called on instances
+   * of the class. Instead, they're called on the class itself.
+   */
+
+  static learn() {
+    console.log("He learns programming daily");
+  }
 }
+
+// instance of Student class
+const ankur = new Student("Ankur");
+ankur.study();
+
+// static methods and properties are called on the class itself
+Student.learn();
 
 // teacher class inheriting properties of students class
 class Teacher extends Student {
@@ -20,8 +46,6 @@ class Teacher extends Student {
   }
 }
 
-// const ankur = new Student("Ankur");
-// ankur.study();
-
+// instance of Teacher class
 const mosh = new Teacher("Mosh", "Programming");
 mosh.teach();
