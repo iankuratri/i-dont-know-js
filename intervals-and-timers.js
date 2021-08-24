fb39034651d0309d1d4aadf3e0205aa44fee1f0d
Ankur Atri <ankuratri.in@gmail.com>
2021-08-24 21:35:45 +0530

    some noteworthy points added in Timers and intervals

--- intervals-and-timers.js
+++ intervals-and-timers.js
@@ -1,15 +1,31 @@
 /**
- * setTimeout allows us to run a function once after the interval of time.
+ * setTimeout
+ *
+ * allows us to run a function once after the interval of time.
  */
 
 function sayHi() {
-  console.log("Hello");
+  console.log("Hi");
 }
 
 setTimeout(sayHi, 100);
 
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+
+setTimeout(sayHello, 200, "Ankur");
+
+const sayHelloAgain = setTimeout(sayHello, 300, "Ankur");
+
+// above timeout will be cleared before execution
+clearTimeout(sayHelloAgain);
+
 /**
- * setInterval allows us to run a function repeatedly,
+ * setInterval
+ *
+ * allows us to run a function repeatedly,
+ *
  * starting after the interval of time, then repeating continuously at that interval.
  */
 
@@ -40,3 +56,44 @@ function setImmediateInterval(funcToRun, delay) {
 }
 
 setImmediateInterval(sayBye, 20000);
+
+/**
+ * Noteworthy points
+ *
+ * 1. Timers and intervals are not part of JavaScript itself. They are implemented
+ * by the browser. setTimeout and setInterval are basically names given to that
+ * functionality in JavaScript.
+ *
+ * 2. Duration parameter is the minimum delay, not guaranteed delay.
+ *
+ * 3. It is possible to achieve the same effect as setInterval with a recursive setTimeout.
+ */
+
+/**
+ * Recursive setTimeout
+ *
+ * 1. Duration is guaranteed between executions.
+ *
+ * Irrespective of how long the code takes to run, the interval will remain the same.
+ *
+ * You can calculate a different delay before running each iteration.
+ */
+
+setTimeout(function run() {
+  console.log("setTimeout - yolo");
+  setTimeout(run, 100);
+}, 100);
+
+/**
+ * setInterval - to achieve same functionality as above
+ *
+ * 1. The duration interval includes the time taken to execute the code you want to run.
+ *
+ * The code takes 40ms to run, the interval is 60 ms.
+ *
+ * The code takes 50ms to run, the interval is 50 ms.
+ */
+
+setInterval(function run() {
+  console.log("setInterval - yolo");
+}, 100);
