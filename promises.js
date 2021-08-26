1aa7b75bda444d7cb83cf90fb46886bfb7a3b07b
Ankur Atri <ankuratri.in@gmail.com>
2021-08-26 18:49:11 +0530

    Promise - Static methods added with example

--- promises.js
+++ promises.js
@@ -43,3 +43,81 @@ const cookGarlicBread = new Promise((resolve, reject) => {
 });
 
 cookGarlicBread.catch((err) => console.log(err));
+
+/**
+ * Both then and catch methods return promises
+ *
+ * then() and catch() methods can be chained in JavaScript
+ */
+
+/**
+ * Promise - Static methods
+ */
+
+/**
+ * Promise.all()
+ *
+ * The Promise.all() method takes an iterable of promises as an input, and returns
+ * a single Promise that resolves to an array of the results of the input promises.
+ *
+ * This returned promise will resolve when all of the input's promises have resolved,
+ * or if the input iterable contains no promises.
+ *
+ * It rejects immediately upon any of the input promises rejecting or non-promises
+ * throwing an error, and will reject with this first rejection message / error.
+ */
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "foo");
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+// expected output: Array [3, 42, "foo"]
+
+/**
+ * Promise.allSettled()
+ *
+ * The Promise.allSettled() method returns a promise that resolves after all of
+ * the given promises have either fulfilled or rejected, with an array of objects
+ * that each describes the outcome of each promise.
+ */
+
+const promise4 = Promise.resolve(3);
+const promise5 = new Promise((resolve, reject) =>
+  setTimeout(reject, 100, "foo")
+);
+const promises = [promise4, promise5];
+
+Promise.allSettled(promises).then((results) =>
+  results.forEach((result) => console.log(result))
+);
+
+// expected output:
+// { status: 'fulfilled', value: 3 }
+// { status: 'rejected', reason: 'foo' }
+
+/**
+ * Promise.race()
+ *
+ * The Promise.race() method returns a promise that fulfills or rejects as soon as
+ * one of the promises in an iterable fulfills or rejects, with the value or reason
+ * from that promise.
+ */
+
+const promise6 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, "one");
+});
+
+const promise7 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "two");
+});
+
+Promise.race([promise6, promise7]).then((value) => {
+  console.log(value);
+  // Both resolve, but promise2 is faster
+});
+// expected output: "two"
