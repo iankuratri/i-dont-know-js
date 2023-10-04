cf76d51f061724ddc30b7b3fc751475ca6fc8d8e
Ankur Atri <ankuratri.in@gmail.com>
2023-10-04 09:58:07 +0530

    Bridge Pattern added

---
+++ design-patterns/structural/bridge.js
@@ -0,0 +1,101 @@
+/**
+
+Bridge Pattern
+
+https://www.dofactory.com/javascript/design-patterns/bridge
+
+The Bridge pattern allows two components, a client and a service, 
+to work together with each component having its own interface. 
+Bridge is a high-level architectural pattern and its main goal 
+is to write better code through two levels of abstraction. 
+It facilitates very loose coupling of objects
+
+The Bridge pattern is a great pattern for driver development but it is rarely seen in JavaScript.
+
+For eg: Mongoose (driver for MongoDB)
+
+*/
+
+// input devices
+
+var Gestures = function (output) {
+  this.output = output;
+
+  this.tap = function () {
+    this.output.click();
+  };
+  this.swipe = function () {
+    this.output.move();
+  };
+  this.pan = function () {
+    this.output.drag();
+  };
+  this.pinch = function () {
+    this.output.zoom();
+  };
+};
+
+var Mouse = function (output) {
+  this.output = output;
+
+  this.click = function () {
+    this.output.click();
+  };
+  this.move = function () {
+    this.output.move();
+  };
+  this.down = function () {
+    this.output.drag();
+  };
+  this.wheel = function () {
+    this.output.zoom();
+  };
+};
+
+// output devices
+
+var Screen = function () {
+  this.click = function () {
+    console.log("Screen select");
+  };
+  this.move = function () {
+    console.log("Screen move");
+  };
+  this.drag = function () {
+    console.log("Screen drag");
+  };
+  this.zoom = function () {
+    console.log("Screen zoom in");
+  };
+};
+
+var Audio = function () {
+  this.click = function () {
+    console.log("Sound oink");
+  };
+  this.move = function () {
+    console.log("Sound waves");
+  };
+  this.drag = function () {
+    console.log("Sound screetch");
+  };
+  this.zoom = function () {
+    console.log("Sound volume up");
+  };
+};
+
+function run() {
+  var screen = new Screen();
+  var audio = new Audio();
+
+  var hand = new Gestures(screen);
+  var mouse = new Mouse(audio);
+
+  hand.tap();
+  hand.swipe();
+  hand.pinch();
+
+  mouse.click();
+  mouse.move();
+  mouse.wheel();
+}
